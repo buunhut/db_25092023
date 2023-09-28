36e1255ed1986d3aee4da43d2152c055caf0d6a6
zero <zero@zeros-MacBook-Pro.local>
2023-09-28 09:52:35 +0700

    sckey

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,8 @@ import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY, // Thay thế bằng secret key của bạn
+      // secret: process.env.JWT_SECRET_KEY, // Thay thế bằng secret key của bạn
+      secret: 'NDOEJS33', // Thay thế bằng secret key của bạn
       signOptions: { expiresIn: '1d' }, // Thời gian hết hạn của token
     }),
   ],
